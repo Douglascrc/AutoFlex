9ad4bdfc832054a02cdc91c63e4c5d92cba578f6
Douglascrc <douglasbuscam@gmail.com>
2026-02-07 20:34:44 -0300

    First commit

---
+++ src/test/java/br/com/autoflex/AutoflexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autoflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
