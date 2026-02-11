b58b3c9f1cfff9e78e18a481dbdf6492fc1f5de8
Douglascrc <douglasbuscam@gmail.com>
2026-02-11 17:42:20 -0300

    feat: add raw material reducer to the store configuration

--- frontend-ui/src/store/index.ts
+++ frontend-ui/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './slices/productSlice';
+import rawMaterialReducer from './slices/rawMaterialSlice';
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    rawMaterials: rawMaterialReducer,
   },
 });
 
